51c650d467a8bc90c6cfab3d390df56598039e44
chai28 <clomosbog@gmail.com>
2020-01-22 13:51:52 +1300

    updated subscribed policy checking

--- controllers/subscribedPolicyController.js
+++ controllers/subscribedPolicyController.js
@@ -4,21 +4,25 @@ const Company = mongoose.model('Company');
 const policy = mongoose.model('Policy');
 
 exports.subscribedPolicyGet = async (req, res) => {
-    Company.findOne({company_name:req.query.companyName}, function (error, company){
+    Company.findOne({
+        company_name: req.query.company_name
+    }, function (error, company) {
         if (error) {
             console.log("Error: " + error);
         } else {
-                // for( var i=0;i<company.subscribed_policy.length;i++){
-                //        sub_policy[i]=company.sub_policy[i].name; 
-                // }
-               // subscribed_policy = await policy.find().where('_id').in(company.match_policy).exec();
-                sub_policy=company.subscribed_policy;
+            if (company.subscribed_policy !== null) {
                 res.json(company.subscribed_policy);
-                console.log(company.subscribed_policy);
+            } else {
+                res.json({
+                    message: "empty"
+                });
+            }
+
+            console.log(company.subscribed_policy);
         }
     })
 };
 exports.subscribedPolicyPost = (req, res) => {
-    
-    
+
+
 };
