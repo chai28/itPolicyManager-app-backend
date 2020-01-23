5e6bc2f0819febc3d5fa6a6e984f1fb7f5baf388
kevinnnnnnnnnnnnnnnnn <2462053692@qq.com>
2020-01-24 10:13:55 +1300

    nothing

--- controllers/pdfGenerationController.js
+++ controllers/pdfGenerationController.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const Company = mongoose.model('Company');
-const puppeteer = require('puppeteer');
+//const puppeteer = require('puppeteer');
 
 exports.pdfGenerationPost = (req, res) => {
 
-    async function printPDF() {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/dashboard/subscribed-policies', {waitUntil: 'networkidle2'});
-        const pdf = await page.pdf({ 
-            format: 'A4',
-           printBackground: true
-        });
-        await browser.close();
-        return pdf
-      }
-      printPDF.then(pdf => {
-        res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
-        res.send(pdf)
-      })  
+    // async function printPDF() {
+    //     const browser = await puppeteer.launch({ headless: true });
+    //     const page = await browser.newPage();
+    //     await page.goto('http://localhost:3000/dashboard/subscribed-policies', {waitUntil: 'networkidle2'});
+    //     const pdf = await page.pdf({ 
+    //         format: 'A4',
+    //        printBackground: true
+    //     });
+    //     await browser.close();
+    //     return pdf
+    //   }
+    //   printPDF.then(pdf => {
+    //     res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+    //     res.send(pdf)
+    //   })  
   };
