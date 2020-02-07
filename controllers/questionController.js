1fcc31f03f6dd4d37facb0eb3a9c8b5d763c2fbb
chai28 <clomosbog@gmail.com>
2020-02-07 14:05:17 +1300

    added status for notification in questions

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -3,7 +3,7 @@ require("../models/question.model.js");
 const Questions = mongoose.model('Questions');
 
 exports.questionsGet = (req, res) => {
-    Questions.find(function(err, questions) {
+    Questions.find(function (err, questions) {
         if (err) {
             console.log("Error: " + err);
         } else {
@@ -13,20 +13,21 @@ exports.questionsGet = (req, res) => {
 };
 
 exports.questionsPost = (req, res) => {
-    
-     req.body.questionInputs.map((question) => {
-         Questions.findById(question._id, function(err,goalQuestion) {
-             goalQuestion.question_content=question.question_content;
-            for(var i =0; i<goalQuestion.options.length; i++){
+
+    req.body.questionInputs.map((question) => {
+        Questions.findById(question._id, function (err, goalQuestion) {
+            goalQuestion.question_content = question.question_content;
+            for (var i = 0; i < goalQuestion.options.length; i++) {
                 goalQuestion.options[i].name = question.options[i].name;
-                goalQuestion.options[i].policy=question.options[i].policy;
+                goalQuestion.options[i].policy = question.options[i].policy;
             }
             goalQuestion.save()
         });
     })
 
-    
+
     res.send({
+        result: "success",
         message: " Post is working"
     });
 };
