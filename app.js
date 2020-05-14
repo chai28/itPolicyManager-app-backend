24d3f92f132e8444272389845547a171641ca0ea
Anton Rozhanskii <anton_rozhanskii@yahoo.com>
2020-05-15 00:51:30 +1200

    Refactor and format according to WebStorm's default rules

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
 
@@ -8,37 +10,33 @@ var MongoDBStore = require('connect-mongodb-session')(session);
 const app = express();
 const port = process.env.PORT || 5000;
 
-require('dotenv').config();
-
-
 var store = new MongoDBStore({
-  uri: process.env.DATABASE_URL,
-  collection: 'sessions',
-  unset: 'destroy'
+    uri: process.env.DATABASE_URL,
+    collection: 'sessions',
+    unset: 'destroy'
 });
 
 store.on('error', function (error) {
-  console.log(error);
+    console.log(error);
 });
 
 app.use(session({
-  secret: 'Somesercrets',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 1 // 1 day
-  },
-  //store: store,
-  resave: true,
-  saveUninitialized: false
+    secret: 'Somesercrets',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 1 // 1 day
+    },
+    resave: true,
+    saveUninitialized: false
 }));
 
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false 
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 const connection = mongoose.connection;
 
 connection.once('open', function () {
-  console.log("MongoDB database connection established successfully");
+    console.log("MongoDB database connection established successfully");
 })
 
 app.use(cors());
@@ -52,7 +50,7 @@ const errorHandler = require("./controllers/errorHandlingController")
 app.use(errorHandler.NotFound);
 
 app.listen(port, function () {
-  console.log("Server is running on Port: " + port);
+    console.log("Server is running on Port: " + port);
 });
 
 module.exports = app;
